8219239e84f3210ae81ef53b42098d462435573d
Phelix2000 <100480589+Phelix2000@users.noreply.github.com>
2022-03-06 14:32:25 +0300

    addition
    A simple function to add two numbers

---
+++ phelix2000/addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int addition(int,int);
+ int main(){
+int add;
+ add=addition(9,2);
+ cout<<add;
+ return 0;
+ }
+ int addition(int a,int b){
+  return a+b;
+ }
